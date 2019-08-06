42de20c53350877d915b167949c89e2e365f7396
shimmy0619 <sausupo0619@yahoo.co.jp>
2019-08-06 13:26:06 +0900

    清水担当分

---
+++ ch05/ex05/main.py
@@ -0,0 +1,26 @@
+from pprint import pprint
+
+plain = {'a': 1, 'b': 2, 'c': 3}
+
+print(plain)
+pprint(plain)
+
+# {'a': 1, 'b': 2, 'c': 3}
+# {'a': 1, 'b': 2, 'c': 3}
+
+# # test
+# wlist = [
+#     ('Python', 'PythonはPython3使いましょう'),
+#     ('機械学習', '基礎からしっかり学ぼう'),
+#     ('人工知能', 'Artificial intelligence'),
+# ]
+
+# print(wlist)
+# pprint(wlist)
+
+# # [('Python', 'PythonはPython3使いましょう'),
+# #  ('機械学習', '基礎からしっかり学ぼう'), ('人工知能', 'Artificial intelligence')]
+
+# # [('Python', 'PythonはPython3使いましょう'),
+# #  ('機械学習', '基礎からしっかり学ぼう'),
+# # ('人工知能', 'Artificial intelligence')]
